28f0d8ed0341d313494e4c7b759a174e12a6be95
[dyad] <aymmej1920@gmail.com>
2025-11-24 11:22:12 +0100

    [dyad] Added inventory sessions management page - wrote 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import InventoryDashboard from "./pages/InventoryDashboard"; // Import the new dashboard
-import Index from "./pages/Index"; // Keep Index for now, but it won't be the default
+import InventoryDashboard from "./pages/InventoryDashboard";
+import Sessions from "./pages/Sessions"; // Import the new Sessions page
+import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -16,8 +17,9 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<InventoryDashboard />} /> {/* Set InventoryDashboard as the default */}
-          <Route path="/index" element={<Index />} /> {/* Keep Index accessible if needed */}
+          <Route path="/" element={<InventoryDashboard />} />
+          <Route path="/sessions" element={<Sessions />} /> {/* Add route for Sessions page */}
+          <Route path="/index" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
